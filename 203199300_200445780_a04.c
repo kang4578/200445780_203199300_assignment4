c3a2ef9d3bb4815d184c73eb0c13f630ad87e713
YiQian Kang <109631634+kang4578@users.noreply.github.com>
2022-07-20 20:47:29 -0400

    Update 203199300_200445780_a04.c

--- 203199300_200445780_a04.c
+++ 203199300_200445780_a04.c
@@ -13,15 +13,36 @@ void firstFit(int process[processSize], int memory[memorySize]){
   }
 }
 
+
+void bestFit(int process[processListSize], int memory[memoryListSize]){
+  int memoryLen=len(memory);
+  int processLen=len(process);
+  int processInMemoryIndex[processLen]
+  int smallestHoleIndex = 2147483647-1;
+  int smallestHoleSize = 0;
+  for(int processInitialValue=0; processInitialValue<processLen; processInitialValue++){
+      for(int memoryInitialValue=0; memoryInitialValue<allocateLen; memoryInitialValue++){
+        if (memory[memoryInitialValue]>=process[processInitialValue] && memory[memoryInitialValue]<smallestHoleSize){
+          smallestHoleIndex=memoryInitialValue;
+          smallestHoleSize=memory[memoryInitialValue];
+        }
+      }
+    memory[smallestHoleIndex] -= process[processInitialValue];
+    processInMemoryIndex[memoryInitialValue] = processInitialValue;
+  }
+  return processInMemoryIndex[processLen];
+}
+    
+          
+
+
+
         
       
         
 
   
   
-  
-}
-
 
   
 
