a325c45f4c3533710a7465698d400a555b7cf280
YiQian Kang <109631634+kang4578@users.noreply.github.com>
2022-07-20 20:32:08 -0400

    Update 203199300_200445780_a04.c

--- 203199300_200445780_a04.c
+++ 203199300_200445780_a04.c
@@ -1,5 +1,25 @@
 
-void first_fit(){
+void firstFit(int process[processSize], int memory[memorySize]){
+  int memoryLen=len(memory);
+  int processLen=len(process);
+  for(int memoryInitialValue=0; memoryInitialValue<allocateLen; memoryInitialValue++){
+    for(int processInitialValue=0; processInitialValue<processLen; processInitialValue++){
+      if memory[memoryInitialValue] >= process[processInitialValue] {
+        memory[memoryInitialValue] = processInitialValue;
+        memory[memoryInitialValue] = memory[memoryInitialValue] - process[processInitialValue];
+      }
+    }
+    return;
+  }
+}
+
+        
+      
+        
+
+  
+  
+  
 }
 
 
